a456ff024c5018d309ac80f4262ace4383c31a43
Andrew Rivett <veggiefrog@gmail.com>
2015-01-06 11:39:05 -0500

    Moved ui thumbSrc, thumbTitle, thumbID to core

--- ui/feature.js
+++ ui/feature.js
@@ -99,21 +99,6 @@ function ciniki_marketing_feature() {
 			}
 			return true;
 		};
-		this.edit.thumbSrc = function(s, i, d) {
-			if( d.image.image_data != null && d.image.image_data != '' ) {
-				return d.image.image_data;
-			} else {
-				return '/ciniki-mods/core/ui/themes/default/img/noimage_75.jpg';
-			}
-		};
-		this.edit.thumbTitle = function(s, i, d) {
-			if( d.image.name != null ) { return d.image.name; }
-			return '';
-		};
-		this.edit.thumbID = function(s, i, d) {
-			if( d.image.id != null ) { return d.image.id; }
-			return 0;
-		};
 		this.edit.thumbFn = function(s, i, d) {
 			return 'M.startApp(\'ciniki.marketing.featureimages\',null,\'M.ciniki_marketing_feature.edit.addDropImageRefresh();\',\'mc\',{\'feature_id\':M.ciniki_marketing_feature.edit.feature_id,\'feature_image_id\':\'' + d.image.id + '\'});';
 		};
